5988e47e88b8d7fad20523c21441296c97173467
unknown <p.rabbia.3255@vallauri.net>
2026-02-27 10:06:15 +0100

    Aggiunto i primi componenti 27/02/2026

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class HomeComponent {}
