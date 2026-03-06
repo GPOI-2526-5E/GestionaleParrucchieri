ec6ef5533013a7e56e5d9c5d156d98cdf2b06e99
paolorabbia <p.rabbia.3255@vallauri.edu>
2026-03-06 10:08:33 +0100

    creazione navbar

---
+++ client/src/app/features/navbar.component/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '../../../../node_modules/@angular/common/common_module.d';
+
+@Component({
+  selector: 'app-navbar.component',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+
+}
