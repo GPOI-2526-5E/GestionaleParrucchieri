d0cdec46c4b47709a7f87a91b2cdb14e32280295
Filippo <f.porro.3249@vallauri.edu>
2026-03-10 18:14:11 +0100

    navbar e body aggiornati(pippo)

--- client/src/app/features/navbar.component/navbar.component.ts
+++ client/src/app/features/navbar.component/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 }
