06effbdc9afe21c4840ab55db14e430718a05843
giuseppearmentano <g.armentano.2955@vallauri.edu>
2026-03-13 11:00:45 +0100

    product list + product card

--- client/src/app/features/products/products-list.component/products-list.component.ts
+++ client/src/app/features/products/products-list.component/products-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar.component/navbar.component';
+import { AiChatDrawerComponent } from "../../ai-chat-drawer.component/ai-chat-drawer.component";
 
 
 @Component({
   selector: 'app-products-list.component',
-  imports: [],
+  imports: [AiChatDrawerComponent, NavbarComponent],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css',
 })
 export class ProductsListComponent {
-  
+
 }
