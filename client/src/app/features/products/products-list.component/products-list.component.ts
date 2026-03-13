905620a25977bb8aa5aa8db63dab8414ddd2ce50
giuseppearmentano <g.armentano.2955@vallauri.edu>
2026-03-13 11:01:01 +0100

    dd

--- client/src/app/features/products/products-list.component/products-list.component.ts
+++ client/src/app/features/products/products-list.component/products-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar.component/navbar.component';
 import { AiChatDrawerComponent } from "../../ai-chat-drawer.component/ai-chat-drawer.component";
-
+import { ProductCardComponent } from '../product-card.component/product-card.component';
 
 @Component({
   selector: 'app-products-list.component',
-  imports: [AiChatDrawerComponent, NavbarComponent],
+  imports: [AiChatDrawerComponent, NavbarComponent, ProductCardComponent],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css',
 })
