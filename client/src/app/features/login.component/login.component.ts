afec49017ee43a5791abf4c32a779ffc573f34b8
Filippo <f.porro.3249@vallauri.edu>
2026-03-20 10:34:46 +0100

    grafica login

--- client/src/app/features/login.component/login.component.ts
+++ client/src/app/features/login.component/login.component.ts
@@ -1,32 +1,71 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login.component',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   standalone: true
 })
-export class LoginComponent {
-  email = '';
-  password = '';
+export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
 
-  constructor(private auth: AuthService, private route: Router){
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
+  constructor(
+    private auth: AuthService,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    const savedEmail = localStorage.getItem('rememberedEmail');
+
+    if (savedEmail) {
+      this.email = savedEmail;
+      this.rememberMe = true;
+    }
   }
-  login(){
-    console.log(this.auth.login(this.email, this.password));
 
-    this.auth.login(this.email, this.password)
-    .subscribe(//funzione che intercetta la risposta nel server
-      res => {
+  goBack(): void {
+    window.history.back();
+  }
+
+  login(): void {
+    if (!this.email || !this.password) {
+      this.errorMessage = 'Inserisci email e password.';
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.auth.login(this.email, this.password).subscribe({
+      next: (res) => {
         this.auth.saveToken(res.token);
+
+        if (this.rememberMe) {
+          localStorage.setItem('rememberedEmail', this.email);
+        } else {
+          localStorage.removeItem('rememberedEmail');
+        }
+
         this.route.navigate(['/products']);
+      },
+      error: (err) => {
+        console.error('Errore login:', err);
+        this.errorMessage = 'Email o password non validi.';
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
       }
-    )
+    });
   }
 }
