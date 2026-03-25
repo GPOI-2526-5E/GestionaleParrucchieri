f73f4d83411885bd20c5e68b963aa198bc1615e3
giuseppearmentano <g.armentano.2955@vallauri.edu>
2026-03-25 13:28:01 +0100

    qta prod list

--- client/src/app/features/prodotti/product-card.component/product-card.component.ts
+++ client/src/app/features/prodotti/product-card.component/product-card.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ChangeDetectorRef
+} from '@angular/core';
+
 import { Prodotto } from '../../../services/prodotto';
 import { CurrencyPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -14,13 +21,49 @@ import { CommonModule } from '@angular/common';
 export class ProductCardComponent {
 
   @Input() product!: Prodotto;
-
   @Output() addProductToCart = new EventEmitter<Prodotto>();
 
-  constructor() { }
+  showError = false;
+  shakeAnimation = false;
+  errorMessage = '';
+  private alertTimeout: any;
+
+  constructor(private cdr: ChangeDetectorRef) { }
 
   addToCart() {
-    console.log(`Aggiunto al carrello: ${this.product.nome}`);
+    const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+
+    const existingProduct = cart.find((p: Prodotto) => p.id === this.product.id);
+
+    const currentQuantity = existingProduct ? (existingProduct.quantita || 1) : 0;
+
+    if (currentQuantity >= this.product.qta) {
+      this.showAlert(
+        `Limite massimo raggiunto per ${this.product.nome}: ${this.product.qta} disponibili`
+      );
+      return;
+    }
+
     this.addProductToCart.emit(this.product);
+  }
+
+  showAlert(message: string) {
+    this.errorMessage = message;
+    this.showError = true;
+
+    this.shakeAnimation = false;
+    this.cdr.detectChanges();
+
+    setTimeout(() => {
+      this.shakeAnimation = true;
+      this.cdr.detectChanges();
+    }, 10);
+
+    clearTimeout(this.alertTimeout);
+
+    this.alertTimeout = setTimeout(() => {
+      this.showError = false;
+      this.cdr.detectChanges();
+    }, 2500);
   }
 }
