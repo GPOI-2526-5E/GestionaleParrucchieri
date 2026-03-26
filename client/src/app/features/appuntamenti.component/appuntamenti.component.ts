39b60b01ad1c838dc32c359ff1d4e3f5ddd2e89e
unknown <p.rabbia.3255@vallauri.net>
2026-03-26 11:46:14 +0100

    appuntamenti

---
+++ client/src/app/features/appuntamenti.component/appuntamenti.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appuntamenti.component',
+  imports: [],
+  templateUrl: './appuntamenti.component.html',
+  styleUrl: './appuntamenti.component.css',
+})
+export class AppuntamentiComponent {
+
+}
